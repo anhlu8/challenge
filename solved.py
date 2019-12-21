f3005906f6e7f0f9fae0e2c8085c2e3cbf6c0892
Anh Lu <anhlu8@outlook.com>
2019-12-20 21:54:17 -0500

    done

--- solved.py
+++ solved.py
@@ -1,16 +1,22 @@
 array = [4,1,3,4,2]
-# array = [1,2,0]
 
 
-def my_count(my_list):
-    my_new_list = [my_list[0]]
-    for x, el in enumerate(my_list):
-        my_new_list.append(my_list[my_new_list[x]])
-    return len(set(my_new_list))
-
-print(my_count(array))
-        
+def shuffle(my_list):
+    new_array=[my_list[0]]
+    for x in range(len(my_list)-1):
+        new_array.append(my_list[new_array[-1]])
+    return len(set(new_array))
 
 
 
+print(shuffle(array))
+
+
+# I changed my answers to this question multiple times. It took me more than half a day to really understand the question. 
+# I think what had made me confused was this sentence: "​It then uses that value as the index for the next value to return, and so on​."
+# I think I would have understood the problem better if that sentence was written: "It then uses that value as the index of the next value to return, and so on​."
+
+# Part b:
+# I haven't worked a lot with big O, but from my research I think using only a constant amount of additional memory would not work for part a. 
+# I think since the input is a list of size N, which means the total Space Complexity would be equal to N plus constant space in other words using only a constant amount of additional memory wouldn't work.
 
--- solved3.py
+++ solved3.py
@@ -1,5 +1,15 @@
-import csv
 import pandas as pd
 
 my_csv = pd.read_csv('file.csv')
-columnA = my_csv[]
+columnA = my_csv['A'].tolist()
+columnB = my_csv['B'].tolist()
+
+for i in columnB:
+    if i not in columnA:
+        print(i)
+        
+for i in range(1,1000001):
+    if i not in columnA and i not in columnB:
+        print(i)
+    
+# For part c, I think if the length of the CSV doubled, the memory usage would also double in auxiliary space
